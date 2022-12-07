31e27316fb889c94a53632d87901e64ea91ee1d8
MMijoc <markomijoc18@gmail.com>
2022-12-07 16:23:31 +0100

    Added a way to generate a simple ppm image

--- RayTracing/RayTracing/Source.cpp
+++ RayTracing/RayTracing/Source.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include "Source.h"
+
 
 int main()
 {
-	std::cout << "Hello world";
+    CreateImageFile();
+    return 0;
+}
 
-	return 0;
+void CreateImageFile()
+{
+    // Image
+    const int image_width = 256;
+    const int image_height = 256;
+
+    std::ofstream ppmImageFile;
+    ppmImageFile.open("image.ppm");
+
+    // Render
+    ppmImageFile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush; // progress indicator
+        for (int i = 0; i < image_width; ++i) {
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            ppmImageFile << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+    std::cerr << "\nDone.\n";
+
+    ppmImageFile.close();
 }

e7f13be066e6e7a000426fba54cb3e5f45829bb2
MMijoc <markomijoc18@gmail.com>
2022-12-17 15:14:43 +0100

    Improved readability

--- RayTracing/RayTracing/Source.cpp
+++ RayTracing/RayTracing/Source.cpp
@@ -7,14 +7,11 @@
 #include "Ray.h"
 
 using namespace RayTracing;
-Color RayColor(const Ray& ray);
-bool HitSphere(const Point3& center, double radius, const Ray& ray);
 
 
 int main()
 {
 	CreateImageFile();
-
 	return 0;
 }
 
@@ -37,7 +34,6 @@ void CreateImageFile()
 	auto origin = Point3(0, 0, 0);
 	auto horizontal = Vector3(viewportWidth, 0, 0);
 	auto vertical = Vector3(0, viewportHeight, 0);
-	//auto lowerLeftCorner = origin - horizontal / 2 - vertical / 2 - Vector3(0, 0, focalLength);
 	Point3 lowerLeftCorner = Vector3(-viewportWidth / 2, -viewportHeight / 2, -focalLength);
 
 
@@ -47,7 +43,6 @@ void CreateImageFile()
 	for (int j = imageHeight - 1; j >= 0; --j) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush; // progress indicator
 		for (int i = 0; i < imageWidth; ++i) {
-			//Color pixelColor(double(i) / (imageWidth - 1), double(j) / (imageHeight - 1), 0.25);
 			auto u = double(i) / (imageWidth - 1);
 			auto v = double(j) / (imageHeight - 1);
 			Ray r(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
@@ -60,27 +55,31 @@ void CreateImageFile()
 	ppmImageFile.close();
 }
 
-Color RayColor(const Ray& ray) {
-
+Color RayColor(const Ray& ray)
+{
 	Point3 sphereCenter = Point3(0, 0, 1);
 	double sphereRadius = 0.5;
+	Color sphereColor = Color(1, 0, 0);
 
-	if (HitSphere(sphereCenter, sphereRadius, ray))
-		return Color(1, 0, 0);
+	if (IsSphereHit(sphereCenter, sphereRadius, ray))
+		return sphereColor;
 
-	Vector3 unitDirection = UnitVector(ray.GetDirection()); // Unit vector of direction
-	double t = 0.5 * (unitDirection.Y() + 1.0); // we scale t betweene 0 and 1, t depends on y axis of vector direction
-	// blendedValue = (1−t)*startValue + t*endValue <- linar interpolation
-	//              multiplyer * endColor            multiplyer * start color            
-	auto rayColor = (1.0 - t) * Color(0.0, 1.0, 0.0) + t * Color(0.5, 0.7, 1.0);
+	Vector3 unitDirection = UnitVector(ray.GetDirection());
+	double t = 0.5 * (unitDirection.Y() + 1.0); // we scale t between 0 and 1
+	Color startColor = Color(0, 0.46, 0.72);
+	Color endColor = Color(0.0, 1.0, 0.0);
+	auto rayColor = (1.0 - t) * endColor + (t * startColor);
+
 	return rayColor;
 }
 
-bool HitSphere(const Point3& center, double radius, const Ray& ray) {
+bool IsSphereHit(const Point3& center, double radius, const Ray& ray)
+{
 	Vector3 oc = ray.GetOrigin() - center;
 	auto a = Dot(ray.GetDirection(), ray.GetDirection());
 	auto b = 2.0 * Dot(oc, ray.GetDirection());
-	auto c = Dot(oc, oc) - radius*radius;
-	auto discriminant = b*b - 4*a*c;
+	auto c = Dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+
 	return (discriminant > 0);
 }
--- RayTracing/RayTracing/Source.h
+++ RayTracing/RayTracing/Source.h
@@ -1,3 +1,5 @@
 #pragma once
 
 void CreateImageFile();
+Color RayColor(const Ray& ray);
+bool IsSphereHit(const Point3& center, double radius, const Ray& ray);

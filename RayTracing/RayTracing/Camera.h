5aee6256081aec0a2472c191f09dc6ae1feedd24
MMijoc <markomijoc18@gmail.com>
2022-12-22 15:00:22 +0100

    Added Camera class

---
+++ RayTracing/RayTracing/Camera.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Source.h"
+
+namespace RayTracing
+{
+	class Camera {
+	public:
+		Camera() {
+			auto aspectRatio = 16.0 / 9.0;
+			auto viewportHeight = 2.0;
+			auto viewportWidth = aspectRatio * viewportHeight;
+			auto focalLength = 1.0;
+
+			origin = Point3(0, 0, 0);
+			horizontal = Vector3(viewportWidth, 0.0, 0.0);
+			vertical = Vector3(0.0, viewportHeight, 0.0);
+			lowerLeftCorner = origin - horizontal / 2 - vertical / 2 - Vector3(0, 0, focalLength);
+		}
+
+		Ray GetRay(double u, double v) const {
+			return Ray(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
+		}
+
+	private:
+		Point3 origin;
+		Point3 lowerLeftCorner;
+		Vector3 horizontal;
+		Vector3 vertical;
+	};
+
+}
--- RayTracing/RayTracing/Source.h
+++ RayTracing/RayTracing/Source.h
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 using std::shared_ptr;
 using std::make_shared;
@@ -15,7 +16,17 @@ const double PI = 3.1415926535897932385;
 
 
 inline double DegreesToRadians(double degrees) {
-    return degrees * PI / 180.0;
+	return degrees * PI / 180.0;
+}
+
+inline double RandomDouble() {
+	// Returns a random real in [0,1).
+	return rand() / (RAND_MAX + 1.0);
+}
+
+inline double RandomDouble(double min, double max) {
+	// Returns a random real in [min,max).
+	return min + (max - min) * RandomDouble();
 }
 
 #include "Ray.h"
